f33c51d0c015f687943384cab77ce8a96b5ee9f0
Arnab-31 <57807670+Arnab-31@users.noreply.github.com>
2022-05-30 13:18:36 +0530

    feat: Database tables added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,56 @@
+import React from 'react';
 import styles from './App.module.css';
 import Nav from './Components/Nav/Nav';
 import Sidebar from './Components/Sidebar/Sidebar';
 import Query from './Components/Query/Query';
+import Table from './Components/Table/Table';
+import makeData from './makeData'
 
 function App() {
+
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: 'Name',
+        columns: [
+          {
+            Header: 'First Name',
+            accessor: 'firstName',
+          },
+          {
+            Header: 'Last Name',
+            accessor: 'lastName',
+          },
+        ],
+      },
+      {
+        Header: 'Info',
+        columns: [
+          {
+            Header: 'Age',
+            accessor: 'age',
+          },
+          {
+            Header: 'Visits',
+            accessor: 'visits',
+          },
+          {
+            Header: 'Status',
+            accessor: 'status',
+          },
+          {
+            Header: 'Profile Progress',
+            accessor: 'progress',
+          },
+        ],
+      },
+    ],
+    []
+  )
+
+  const data = React.useMemo(() => makeData(20), [])
+
+  
   return (
     <div className={styles.App}>
       <div className={styles.nav}>
@@ -11,13 +58,12 @@ function App() {
       </div>
       <div className={styles.container}>
           <Sidebar />
-        <div>
+        <div className={styles.mainContainer}>
+            <div>
+              <Table columns={columns} data={data} />
+            </div>
             <div>
               <Query />
-            </div>
-
-            <div>
-              Rows
             </div>
         </div>
       </div>
